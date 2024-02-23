11c6c96808966f6e3c482984887806f89e0fff50
Paul <p.rossiter83@gmail.noreply.github.com>
2024-02-23 15:26:49 +0000

    [Feature] spining cube added & styled

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,8 +1,40 @@
+import {
+    faHtml5,
+    faNode,
+    faReact,
+    faGitAlt,
+    faSass,
+    faCss3Alt,
+} from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './index.scss';
 
 const Home = () => {
     return ( 
-        <div>HOME</div>
+        <div className='home-page'>
+            <div className="stage-cube-cont">
+                <div className="cubespinner">
+                    <div className="face1">
+                        <FontAwesomeIcon icon={faHtml5}/>
+                    </div>
+                    <div className="face2">
+                        <FontAwesomeIcon icon={faCss3Alt}/>
+                    </div>
+                    <div className="face3">
+                        <FontAwesomeIcon icon={faReact} />
+                    </div>
+                    <div className="face4">
+                        <FontAwesomeIcon icon={faNode} />
+                    </div>
+                    <div className="face5">
+                        <FontAwesomeIcon icon={faGitAlt} />
+                    </div>
+                    <div className="face6">
+                        <FontAwesomeIcon icon={faSass}/>
+                    </div>
+                </div>
+            </div>
+        </div>
      );
 }
  
