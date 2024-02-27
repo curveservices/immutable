cfa09a8fc2edf31800177596d82e38789d3a4e6e
Paul <p.rossiter83@gmail.noreply.github.com>
2024-02-27 13:51:17 +0000

    [Feature] services page added

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,16 @@ import NotFound from "./components/404";
 import About from "./components/about";
 import Home from "./components/home";
 import Layout from "./components/layout";
+import Services from "./components/Services-page";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route element={<Layout />}>
             <Route path="/" element={<Home />} />
-            <Route path="about" element={<About/>}/>
+            <Route path="about" element={<About />} />
+            <Route path="services" element={<Services />}>
+                
+            </Route>
         <Route>
             <Route path="*" element={<NotFound/>} />
         </Route>
