d721237d920b865ed967eae7e0556ddec7f838e5
Paul <p.rossiter83@gmail.noreply.github.com>
2024-02-28 15:19:48 +0000

    [Feature] card component created and styledfor featured work

--- src/components/cards/index.jsx
+++ src/components/cards/index.jsx
@@ -1,19 +1,52 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { db } from '../../firbase';
+import { getDocs, collection } from 'firebase/firestore';
+import Button from '../button';
 import './index.scss';
+
 
 const Card = () => {
     const [card, setCard] = useState([]);
 
+    const fetchData = async () => {
+        try {
+            const workData = [];
+            const snapshot = await getDocs(collection(db, 'work'))
+            snapshot.forEach((element) => {
+                const data = element.data()
+                workData.push({
+                    id: element.id,
+                    name: data.name,
+                    img: data.img,
+                    desc: data.desc,
+                    live: data.live
+                });
+            });
+            setCard(workData);
+        } catch (err) {
+            alert('Error fetching data', err);
+        }
+    };
+    
+    useEffect(() => {
+        fetchData()
+    }, []);
+    
     return (
         <div className="card-container">
             {card.map((item) => {
+                return (
                 <div className="card" key={item.id}>
                     <img src={item.img} alt="feature image" className='card-image' />
                     <div className="content">
                         <h3 className='name'>{item.name}</h3>
                         <p className="desc">{item.desc}</p>
+                        <button className="btn" onClick={() => window.open(item.live)}>
+                            VIEW
+                        </button>
                     </div>
                 </div>
+                )
             })}
         </div>
     );
