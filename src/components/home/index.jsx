a194cde8d8d87c4b6bb1b13286848e9d51dd857c
Paul <p.rossiter83@gmail.noreply.github.com>
2024-02-28 11:49:23 +0000

    [Feature] cta & foot added

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,12 +1,14 @@
 import Button from '../button/index'
 import CubeSpinner from '../Animations/cubespinner';
-import './index.scss';
 import StaticCubes from '../Animations/staticCubes';
 import { accordionData } from '../utils/accordionData';
 import Accordion from '../utils/Accordion';
 import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import Symbols from '../Animations/symbols';
 import Card from '../cards';
+import './index.scss';
+import Socials from '../socials';
 
 
 const Home = () => {
@@ -15,9 +17,12 @@ const Home = () => {
     const handleAccordionClick = (accordionId) => {
         setActiveAccordion((prev) => (prev === accordionId ? null : accordionId));
     }
+
+    const year = new Date().getFullYear();
+
     return ( 
         <div className='home-page'>
-            <section className='right-hero'>
+            <section className='hero'>
                 <div className="text-box">
                     <h1 className='main-title'>Web Develoment Agency</h1>
                     <p>Immutable Studio is a leading web development agency comprising seasoned web designers,
@@ -32,7 +37,7 @@ const Home = () => {
                     <CubeSpinner />
                 </div>
             <section className='second-section'>
-                <h1 className='service-title'>SERVICES</h1>
+                <h1 className='title'>SERVICES</h1>
                 <div className="service-container">
                     <div className="accordion" >
                         {accordionData.map(({ title, subtitle, content, id, link }) => (
@@ -52,8 +57,58 @@ const Home = () => {
                 <StaticCubes />
             </section>
             <section className='third-section'>
-                <h1 className='feature-title'>FEATURED WORK</h1>
+                <h1 className='title feature-title'>FEATURED WORK</h1>
+                <Card />
+                <div className="btn-container">
+                    <Button text='view more' link='' />
+                </div>
             </section>
+            <section className='cta'>
+                <h2>Ready to get started?</h2>
+                <h3>Contact us to discuss your project, either a discovery call 
+                    with us or download our website starter kit to get started.
+                </h3>
+                <div className="btn-container">
+                    <Button text='book a call' link=''/>
+                    <Button text='click to download' link='' />
+                </div>
+            </section>
+            <footer>
+                <h3>&#123; Immutable Studio &#125;</h3>
+                <div className="link-container">
+                    <div className="studio">
+                        <div>Studio</div>
+                        <ul className="site-links">
+                            <li><NavLink />Home</li>
+                            <li><NavLink />About</li>
+                            <li><NavLink />Services</li>
+                            <li><NavLink />Contact</li>
+                        </ul>
+                    </div>
+                    <div className="lead">
+                        <div>Lead Dev</div>
+                        <ul className="site-links">
+                            <li><NavLink />Portfolio</li>
+                            <li><NavLink />About</li>
+                            <li><NavLink />Contact</li>
+                        </ul>
+                    </div>
+                    <div className="experts">
+                        <div>Experts in</div>
+                        <ul>
+                            <li>Hospitality</li>
+                            <li>Tutoring</li>
+                            <li>B 2 B</li>
+                            <li>Design</li>
+                        </ul>
+                    </div>
+                    <div className="social-container">
+                        <p>Follow us</p>
+                        <Socials />
+                    </div>
+                    <p>Copyyright &copy; {year} Immutable Studio </p>
+                </div>
+            </footer>
         </div>
      );
 }
