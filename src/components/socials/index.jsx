2f65e7dd1e66f9d56426b31bd733a2c3e2f4e40a
Paul <p.rossiter83@gmail.noreply.github.com>
2024-02-29 10:57:12 +0000

    [Refactored]

--- src/components/socials/index.jsx
+++ src/components/socials/index.jsx
@@ -1,8 +1,30 @@
+import { faFacebook, faLinkedin, faSquareXTwitter } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from 'react-router-dom';
 import './index.scss';
 
 const Socials = () => {
     return ( 
-        <div className="container"></div>
+        <div className="container">
+            <p>Follow Us!</p>
+            <ul className="iconList">
+                <li className="icons">
+                    <a href='' target='_blank' rel='noreferer'>
+                        <FontAwesomeIcon icon={faFacebook} />
+                    </a>
+                </li>
+                <li className="icons">
+                    <Link to='' target='_blank' rel='noreferer'>
+                        <FontAwesomeIcon icon={faLinkedin} />
+                    </Link>
+                </li>
+                <li className="icons">
+                    <Link to='' target='_blank' rel='noreferer'>
+                        <FontAwesomeIcon icon={faSquareXTwitter} />
+                    </Link>
+                </li>
+            </ul>
+        </div>
      );
 }
  
