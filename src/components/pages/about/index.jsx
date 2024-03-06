37b6d33084ba4df4aad3399bba0823b3f0280efb
Paul <p.rossiter83@gmail.noreply.github.com>
2024-03-06 17:02:26 +0000

    [Refactored] copy changed and styled

--- src/components/pages/about/index.jsx
+++ src/components/pages/about/index.jsx
@@ -4,6 +4,26 @@ import './index.scss'
 const About = () => {
     return ( 
         <div className='about-page'>
+            <section className="hero">
+                <div className="inner-hero">
+                    <div className="text-box">
+                        <h1>Who are we?</h1>
+                        <p>We are a proud south east London design studio specialising in web development, design, social media marketing,
+                            and AI chat bots. We aim to apply the best of our skills and enthusiasm to your challenges and ideas.
+                        </p>
+                        <p>We are passionate about tech, design, problem solving and business.</p>
+
+                        <p>Paul created the company in 2022 and has been delivering amazing websites and AI solutions ever since.
+                            His nephew Harvey fresh from the Uni of Westminster has come on board to deliver social media marketing and strategies for our clients
+                        </p>
+                    </div>
+                </div>
+            </section>
+            <section className="second-section">
+
+
+                
+            </section>
         </div>
      );
 }
