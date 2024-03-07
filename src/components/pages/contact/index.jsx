985ab414abeb043c758a9415762b9ab2be31319e
Paul <p.rossiter83@gmail.noreply.github.com>
2024-03-07 15:32:24 +0000

    [Feature] contact form, map added. styles inserted

--- src/components/pages/contact/index.jsx
+++ src/components/pages/contact/index.jsx
@@ -1,9 +1,113 @@
 import './index.scss';
+import emailjs from '@emailjs/browser';
+import { useLayoutEffect, useRef } from 'react';
+import Socials from '../../socials';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+
 
 const Contact = () => {
+  const refForm = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs
+      .sendForm(
+        'p.rossiter83@gmail.com',
+        'contact_form',
+        refForm.current,
+        'w1ULsXxchzrjhuclR'
+      )
+      .then(
+        () => {
+          alert('Message successfully sent!')
+          window.location.reload(false)
+        },
+        () => {
+          alert(' Failed to send your message, pleease try again')
+        }
+      )
+  }
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0)
+  });
   return ( 
-    <>
-    </>
+    <div className='contact-page'>
+      <section className="text-box">
+        <h1>CONTACT US</h1>
+        <p>Ready to get your project started? 
+          wheather it's a large project, persnoal portfolio, landing page or website chat bot, 
+          We are able to help.</p>
+        <p>
+          However, don't hesitate to contact us with other requests or questions
+          using the form below.
+        </p>
+        <div className="contact-form">
+          <form ref={refForm} onSubmit={sendEmail}>
+            <ul>
+              <li className='half'>
+                <input
+                  type="text"
+                  name='name'
+                  placeholder='Full Name'
+                  required
+                />
+              </li>
+              <li className='half'>
+                <input
+                  type="email"
+                  name='email'
+                  placeholder='Email address'
+                  required
+                />
+              </li>
+              <li>
+                <input
+                  type="text"
+                  name='subject'
+                  placeholder='Subject'
+                  required
+                />
+              </li>
+              <li>
+                <textarea
+                  name="message"
+                  placeholder="Your message"
+                  cols="30"
+                  rows="10"
+                ></textarea>
+              </li>
+              <li>
+                <input
+                  type="submit"
+                  className='flat-button'
+                  value='send'
+                />
+              </li>
+            </ul>
+          </form>
+        </div>
+      </section>
+    
+      <div className="info-map">
+        Immutable Studio
+        <br />
+        London, SE10
+        <br />
+        United Kingdom
+        <br />
+        <Socials />
+        <span>info@immutable-studio.com</span>
+      </div>
+      <div className="map-wrap">
+        <MapContainer center={[51.5, 0.0192269]} zoom={13}>
+          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+          <Marker position={[51.4852, 0.0175]}>
+            <Popup>We are here</Popup>
+          </Marker>
+        </MapContainer>
+      </div>
+    </div>
    );
 }
  
