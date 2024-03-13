69ffd16c2b8f56eadf1e75d8bcd7aef0436158cf
Paul <p.rossiter83@gmail.noreply.github.com>
2024-03-13 14:00:23 +0000

    [Refactored] data  changes

--- src/components/utils/accordionData/index.jsx
+++ src/components/utils/accordionData/index.jsx
@@ -5,7 +5,7 @@ export const accordionData = [
     title: "WEB DEVELOPMENT",
     subtitle: "JavaScript - HTML 5 - CSS 3 - React - Node.js",
     content:
-      "We will create custom corporate & startup websites to suit your needs. If you're looking for a landing page or promo site we have you covered. Our services also iclude CMS platforms such as wordpress, squarespace and webFlow.",
+      "We will create custom corporate & startup websites to suit your needs. If you're looking for a landing page, promo site or a professional multi-page site we have you covered. Our services also iclude CMS platforms such as WordPress, Squarespace and WebFlow.",
   },
   // {
   //   id: 1,
@@ -17,26 +17,26 @@ export const accordionData = [
   // },
   {
     id: 2,
-    link: "packages",
-    title: "PACKAGES",
-    subtitle: "Multi page websites - Landing pages - Promo sites - SPA's",
-    content:
-      "At Immutable Studio we believe in transparency and building strong relationships with our clients. That's why we offer straightforward pricing for our services. We understand the importance of budgeting, which is why we provide a breakdown of our pricing. All of our packages can be expanded as your business grows so that your site always fits your needs.",
-  },
-  {
-    id: 3,
     link: "chat-bots",
     title: "AI CHAT BOTS",
     subtitle: "VoiceFlow - Botpress - Tidio - Zapier",
     content:
-      "If you are looking for AI sulotions for your business we can help alleviate your pain points with an online chat bot. We will help you automate and outsource parts of your day to day work with the latest AI technology.",
+    "If you are looking for AI sulotions for your business we can help alleviate your pain points with an online chat bot. We will help you automate and outsource parts of your day to day work with the latest AI technology.",
   },
   {
-    id: 4,
+    id: 3,
     link: "social-media-marketing",
     title: "SOCIAL MEDIA MARKETING",
     subtitle: "Facebook - LinkedIn - X - Instagram",
     content:
       "Our experienced media content team are on hand to help you set up ad campaigns and social content. We will help drive your social projects using latest trends and analyics",
+  },
+  {
+    id: 4,
+    link: "packages",
+    title: "PACKAGES",
+    subtitle: "Multi page websites - Landing pages - Promo sites - SPA's - Chatbots - Social Media" ,
+    content:
+      "At Immutable Studio we believe in transparency and building strong relationships with our clients. That's why we offer straightforward pricing for our services. We understand the importance of budgeting, which is why we provide a breakdown of our pricing. All of our packages can be expanded as your business grows so that your site always fits your needs.",
   },
 ];
