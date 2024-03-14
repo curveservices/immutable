d22c0891c81220e2613b7b9aaaa67de58cc51b00
Paul <p.rossiter83@gmail.noreply.github.com>
2024-03-14 12:45:07 +0000

    [Feature] social page constucted

--- src/components/pages/social-marketing/index.jsx
+++ src/components/pages/social-marketing/index.jsx
@@ -1,7 +1,85 @@
+import { useEffect, useLayoutEffect, useState } from "react";
+import Symbols from "../../Animations/symbols";
+import Button from "../../button";
+import img1 from '../../../assets/images/social.jpeg';
 import "./index.scss";
+import CallToAction from "../../CTA";
 
 const SocialMarketing = () => {
-  return <></>;
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [second, setSecond] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPrice = window.scrollY;
+      const scrollSecond = window.scrollY;
+      setSecond(scrollSecond > 150)
+      setIsScrolled(scrollPrice > 1000);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+  return (
+    <div className="soical-page">
+      <section className="hero">
+        <div className="inner-hero">
+          <div className="text-box">
+            <h1>SOCIAL MEDIA MARKETING</h1>
+            <p>
+              Our proficient media content team is available to assist in creating ad campaigns
+              and social content, leveraging the latest trends and analytics to propel your social projects forward.
+            </p>
+            <h2>Social media templates start from just £80</h2>
+            <Button text="Speak with us today" />
+          </div>
+          <Symbols />
+        </div>
+      </section>
+      <section className="second-section">
+        <div className="second-inner">
+          <div className={`${second ? "animSecond" : "second-none"}`}>
+            <h1>Elevate social presence with our professional template designs.</h1>
+            <p>
+              In the competitive digital landscape, brand distinction is crucial.
+              Our design agency excels in creating unique social media templates.
+              Leveraging design and branding expertise, we ensure lasting impact,
+              meaningful connections, and ongoing online success for your business.
+            </p>
+          </div>
+          <img src={img1} alt="social media" className={`${second ? "animSecondimg" : "second-none"}`} />
+        </div>
+      </section>
+      <section className="third-section">
+        <div className="third-inner">
+          <div className="text-box">
+            <h1> CHECK OUT OUR RANGE OF SOCIAL MEDIA PACKAGES</h1>
+            <p>
+              We prioritize transparent communication and robust client relationships.
+              Our services offer clear, budget-friendly pricing with detailed breakdowns.
+              With our scalable packages, your website adapts to evolving needs,
+              emphasizing flexibility and alignment with your goals as your business expands.
+            </p>
+          </div>
+        </div>
+        <div
+          className={`${isScrolled ? "contentAnim" : "none"}`}
+          id="social-price"
+        >
+          </div>
+      </section>
+      <section className="cta">
+        <CallToAction />
+      </section>
+    </div>
+  );
 };
 
 export default SocialMarketing;
