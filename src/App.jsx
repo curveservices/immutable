0495546f23827ab677f4975bbfb8be82ba730693
Paul <p.rossiter83@gmail.noreply.github.com>
2024-03-15 17:46:08 +0000

    [Feature] lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,20 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
+import React from "react";
+
 import "./App.scss";
 
 import About from "./components/pages/about";
 import Home from "./components/pages/home";
 import Layout from "./components/layout";
-import Services from "./components/pages/servicesPage";
 import NotFound from "./components/pages/404";
 import Contact from "./components/pages/contact";
-import WebDev from "./components/pages/webDev";
-import Packages from "./components/pages/packages";
-import Chatbots from "./components/pages/chatBots";
-import SocialMarketing from "./components/pages/social-marketing";
+const Services = React.lazy(() => import('./components/pages/servicesPage'))
+const WebDev = React.lazy(() => import('./components/pages/webDev'));
+const Packages = React.lazy(() => import('./components/pages/packages'));
+const Chatbots = React.lazy(() => import('./components/pages/chatBots'));
+const SocialMarketing = React.lazy(() => import('./components/pages/socialMarketing'));
 
 const router = createBrowserRouter(
   createRoutesFromElements(
