425f04a06510df1f56f2723e5502f4ab5a321685
Paul <p.rossiter83@gmail.noreply.github.com>
2024-03-15 19:45:39 +0000

    [Feature] packages page updatted and styled

--- src/components/pages/packages/index.jsx
+++ src/components/pages/packages/index.jsx
@@ -1,7 +1,10 @@
 import { useEffect, useLayoutEffect, useState } from "react";
 import Button from "../../button";
-import img from '../../../assets/images/package.gif'
+import img from '../../../assets/images/packages-unscreen.gif'
+import packageImg from '../../../assets/images/packages.jpeg'
 import "./index.scss";
+import CallToAction from "../../CTA";
+import ChatBotCard from "../../cards/botCard";
 
 const Packages = () => {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -33,13 +36,49 @@ const Packages = () => {
           <div className="text-box">
             <h1>Business Packages</h1>
             <p>
-              bcdslkvlsvbfoheoefbhvo uefrghvhvoihvoiehioerhvoeirhvieorhveriov
+              Seeking AI solutions for your business? Let us ease your challenges with an online chatbot.
+              Our expertise enables automation and outsourcing of daily tasks using cutting-edge AI technology,
+              streamlining your operations for increased efficiency.
             </p>
             <h2>We build tailer made business packagees starting from just £999</h2>
             <Button text='Speak to us today' link='/contact' />
           </div>
-          <img src="" alt="" />
+          <img src={img} alt="package image" className="package-gif"/>
         </div>
+      </section>
+      <section className="second-section">
+        <div className="second-inner">
+          <div className={`${second ? "animSecond" : "second-none"}`}>
+            <h1>Are you in need of full a full online web solutions?</h1>
+            <p>
+             CHANGE COPY!!!!!!!!!!!!!?
+              Immutable Studio excels in crafting inviting and functional chatbots integrated to your website.
+              Our tech experts leverage cutting-edge AI, technical prowess, and industry insight to optimize sales conversion,
+              ensuring exceptional results aligned with your business objectives.
+            </p>
+          </div>
+          <img src={packageImg} alt="packagge image" className={`${second ? "animSecondimg" : "second-none"}`}/>
+        </div>
+      </section>
+      <section className="third-section">
+        <div className="third-inner">
+          <div className="text-box">
+            <h1>CHECK OUT OUR CUSTOM BUSINESS PACKAGES</h1>
+            <p>
+              We prioritize transparent communication and strong client bonds.
+              Our services offer clear, budget-friendly pricing with detailed breakdowns.
+              Through scalable packages, we ensure your website chatbot evolves alongside your business,
+              emphasizing adaptability and alignment with your goals as you expand.
+            </p>
+          </div>
+          <div
+            className={`${isScrolled ? "contentAnim" : "none"}`} id="package-price">
+            <ChatBotCard/>
+            </div>
+        </div>
+      </section>
+      <section className="cta">
+        <CallToAction />
       </section>
     </div>
   );
