e85d445987a934230af807e64264dc6e6bef595a
Paul <p.rossiter83@gmail.noreply.github.com>
2024-03-20 10:26:07 +0000

    [Refactored] lazy removed

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
-import React, { lazy } from "react";
 
 import "./App.scss";
 
@@ -13,14 +12,11 @@ import Home from "./components/pages/home";
 import Layout from "./components/layout";
 import NotFound from "./components/pages/404";
 import Contact from "./components/pages/contact";
-import Loader from "react-loaders";
-
-
-const Services = lazy(() => import('./components/pages/servicesPage'))
-const WebDev = lazy(() => import('./components/pages/webDev'));
-const Packages = lazy(() => import('./components/pages/packagesPage'));
-const Chatbots = lazy(() => import('./components/pages/chatBots'));
-const SocialMarketing = lazy(() => import('./components/pages/socialMarketing'));
+import Services from './components/pages/servicesPage';
+import WebDev from './components/pages/webDev';
+import ChatBots from "./components/pages/chatBots";
+import Packages from "./components/pages/packagesPage";
+import SocialMarketing from './components/pages/socialMarketing';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -29,35 +25,14 @@ const router = createBrowserRouter(
         <Route index element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="contact" element={<Contact />} />
-        <Route path="services" element={
-          <React.Suspense fallback={<Loader type="ball-spin-fade-loader" />}>
-            <Services />
-          </React.Suspense>} />
-        <Route path=":id/web-development" element={
-          <React.Suspense fallback={<Loader type="ball-spin-fade-loader" />}>
-            <WebDev />
-          </React.Suspense>
-          } />
-        <Route path=":id/chat-bots" element={
-          <React.Suspense fallback={<Loader type="ball-spin-fade-loader" />}>
-            <Chatbots />
-          </React.Suspense>
-        } />
-        <Route path=":id/packages" element={
-          <React.Suspense fallback={<Loader type="ball-spin-fade-loader" />}>
-            <Packages />
-          </React.Suspense>
-        } />
-        <Route path=":id/social-media-marketing" element={
-          <React.Suspense fallback={<Loader type="ball-spin-fade-loader" />}>
-            <SocialMarketing />
-          </React.Suspense>
-        } />
-      
-       
+        <Route path="services" element={<Services />} />
+        <Route path=":id/web-development" element={<WebDev />} />
+        <Route path=":id/chat-bots" element={<ChatBots />} />
+        <Route path=":id/packages" element={<Packages />} />
+        <Route path=":id/social-media-marketing" element={<SocialMarketing />} />
       </Route>
       <Route path="*" element={<NotFound />} />
-    </>,
+    </>
   ),
 );
 
