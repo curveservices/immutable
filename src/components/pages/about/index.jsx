7f05db49ed029a861ff88e362100d5148b89282b
Paul <p.rossiter83@gmail.noreply.github.com>
2024-03-23 08:58:45 +0000

    [Refactored]mobilestyling

--- src/components/pages/about/index.jsx
+++ src/components/pages/about/index.jsx
@@ -1,4 +1,4 @@
-import { useLayoutEffect } from "react";
+import { useLayoutEffect, useState } from "react";
 import Button from "../../button";
 import Socials from "../../socials";
 import "./index.scss";
@@ -6,8 +6,23 @@ import Loader from "react-loaders";
 import TeamCard from "../../cards/teamCard";
 import CallToAction from "../../CTA";
 import StaticCubes from "../../Animations/staticCubes";
+import { useEffect } from "react";
 
 const About = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollCard = window.scrollY;
+      setIsScrolled(scrollCard > 350);
+    };
+    window.addEventListener("scroll", handleScroll)
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    }
+  }, []);
+
+
   useLayoutEffect(() => {
     window.scrollTo(0, 0);
   });
@@ -17,7 +32,7 @@ const About = () => {
         <section className="hero">
           <Loader type="ball-spin-fade-loader" />
           <div className="inner-hero">
-            <StaticCubes />
+            
           <div className="text-box">
               <div className="left-hero">
                 <h1 className="title">
@@ -39,13 +54,13 @@ const About = () => {
                 We are passionate about tech, AI, problem solving and
                 business.
               </p>
-
               <p>
                 Immutable Studio was created in 2022 and has been delivering amazing
                 websites and AI solutions ever since. Our Digital team
                 specailise in social media marketing and strategies.
-              </p>
-            </div>
+                </p>
+              </div>
+              <StaticCubes />
           </div>
         </div>
       </section>
@@ -54,7 +69,7 @@ const About = () => {
           <div className="text-box">
             <h1>MEET THE TEAM</h1>
           </div>
-          <div className="team-container">
+          <div className={`${isScrolled ? "contentAnim" : 'none'}`}>
             <TeamCard />
           </div>
         </div>
