ac5f3364388eb2189c56d58b90bdd8444c581e26
Paul <p.rossiter83@gmail.noreply.github.com>
2024-06-18 16:03:09 +0100

    [Refactored] hero section changes removed cube added typewriter effect

--- src/components/pages/home/index.jsx
+++ src/components/pages/home/index.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Loader from "react-loaders";
 import { Helmet, HelmetProvider } from "react-helmet-async";
+import TypewriterComponent from "typewriter-effect";
 
 import Button from "../../button/index";
-import CubeSpinner from "../../Animations/cubespinner";
 import StaticCubes from "../../Animations/staticCubes";
 import { accordionData } from "../../utils/accordionData";
 import Accordion from "../../Accordion";
@@ -12,8 +12,18 @@ import Featured from "../../featured";
 import CallToAction from "../../CTA";
 import "./index.scss";
 
+
 const Home = () => {
+  const [second, setSecond] = useState(false)
   const [activeAccordion, setActiveAccordion] = useState(0);
+
+  useEffect(() => {
+     const handleScroll = () => {
+      const scrollP = window.scrollY;
+      setSecond(scrollP > 350);
+    };
+    window.addEventListener("scroll", handleScroll);
+  }, []);
   const handleAccordionClick = (accordionId) => {
     setActiveAccordion((prev) => (prev === accordionId ? prev : accordionId));
   };
@@ -21,24 +31,44 @@ const Home = () => {
     <>
       <div className="home-page">
         <section className="hero">
-          <div className="right-hero">
             <div className="text-box">
-              <h1 className="main-title">
-                Empowering your Business with Website &amp; AI Solutions
+              <h1 className="main-title" >
+                Empowering your Business with
               </h1>
-              <p>
-                Discover how we can revolutionise your business. Our custom web
+            <div className="typewriter">
+              <TypewriterComponent
+              options={{
+              strings: [
+                "Website Design.",
+                "Web Development.",
+                "AI Solutions.",
+                "Workflow Automation.",
+                "Custom Built GPT's",
+                "Chat Assistants.",
+                "Digital Marketing.",
+                  ],
+                wrapperClassName: 'Typewriter__wrapper',
+              autoStart: true,
+              loop: true,
+            }}
+            />
+            </div>
+            <div className="text">
+               <p>
+                Discover how we can transform your online presence. Our custom web
                 development &amp; AI solutions, including bespoke AI chatbots,
                 and automated workflows are designed to optimise efficiency,
                 reduce costs, and enhance customer engagement.
               </p>
               <p>
                 We are seasoned web developers &amp; Ai specialists who excel in
-                creating custom business websites and workflow solutions. Our
+                creating professional custom websites and workflow solutions. Our
                 team is dedicated to delivering cutting-edge technology tailored
-                to help your company achieve its goals.
+                to help you achieve its goals.
               </p>
 
+            </div>
+             
               <div className="btn-container">
                 <Button link="contact" text="CONTACT US" />
                 <Button
@@ -47,14 +77,13 @@ const Home = () => {
                   noreferer
                 />
               </div>
-            </div>
           </div>
         </section>
 
         <section className="second-section">
           <div className="second-inner">
             <h2 className="title">SERVICES</h2>
-            <p className="text-box">
+            <p className={`text-box ${second ? "anim" : "none"}`}>
               Our services model operates with monthly payments covering hosting
               and maintenance, AI and automation solutions, and digital
               marketing. Service payments are customized based on your specific
@@ -62,10 +91,10 @@ const Home = () => {
               provide comprehensive support, making it easy for your business to
               grow and adapt in the ever-evolving digital landscape.
             </p>
-            <div className="cubes">
+            <div className={`cubes ${second ? "anim" : "none"}`}>
               <StaticCubes />
             </div>
-            <div className="accordion">
+            <div className={`accordion ${second ? "anim" : "none"}`}>
               {accordionData.map(({ title, subtitle, content, id, link }) => (
                 <Accordion
                   key={id}
@@ -92,18 +121,17 @@ const Home = () => {
       </div>
       <div>
         <HelmetProvider>
-              <Helmet>
-          <title>
-            Home | Immutable Studio: Web Development, AI &amp; Automation
-            Solutions Agency
-          </title>
-          <meta
-            name="keywords"
-            content="Website, Website Design, Website Development, Web design, AI Solutions, Chatbot, Empowering, workflows, automated, Business, London, Greenwich London"
-          />
-        </Helmet>
+          <Helmet>
+            <title>
+              Home | Immutable Studio: Web Development, AI &amp; Automation
+              Solutions Agency
+            </title>
+            <meta
+              name="keywords"
+              content="Website, Website Design, Website Development, Web design, AI Solutions, Chatbot, Empowering, workflows, automated, Business, London, Greenwich London"
+            />
+          </Helmet>
         </HelmetProvider>
-        
       </div>
       <Loader type="ball-spin-fade-loader" />
     </>
