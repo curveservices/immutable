df4d6d4ce58b28d60db3688332b985cd8a25aa65
Paul <p.rossiter83@gmail.noreply.github.com>
2024-07-07 14:04:02 +0100

    [Refactored] copy change

--- src/components/pages/featuredWork/index.jsx
+++ src/components/pages/featuredWork/index.jsx
@@ -64,11 +64,12 @@ const FeaturedWork = () => {
             <Client
               name="telegraph-tutoring"
               p="A local tutoring company contacted us for a brand new website. 
-                They wanted a fast, custom-built site developed with the latest front-end tech, using a database for learning resoures to be access by parents.
-                We built the site to be fully optimized for mobile and tablet. We ensured this site was SEO-friendly, 
+                They wanted a fast, custom-built website developed with the latest front-end tech, using a database for learning resources to be accessed by parents.
+                We built their website to be fully optimised for mobile and tablet. We ensured this site was SEO-friendly, 
                 complete with a comprehensive sitemap and google analytics"
-              p2="We work with them to get the colour palette, fonts, and images just right to create a visually appealing and engaging user experience. 
-                  We where provided with their company copy to maintain brand consistency and deliver clear messaging. The result is a modern, efficient, and user-friendly website that enhances MorfBot's digital presence."
+              p2="We worked with them to get the colour palette, fonts, and images to create a visually appealing and engaging user experience. 
+                  We where provided with their company copy to maintain brand consistency and deliver clear messaging. 
+                  The result is a modern, efficient, and user-friendly website to enhance their digital presence."
               video={tutor}
               link="https://telegraphhilltutoring.netlify.app/"
               p3=""
@@ -80,10 +81,11 @@ const FeaturedWork = () => {
             <Client
               name="morfbot"
               p="An innovative AI automation agency approached us for a complete redesign of their website. 
-                            They wanted to move from WordPress to a custom-built site developed with React. Our team ensured the site was fully mobile-optimized and SEO-friendly, 
-                            complete with a comprehensive sitemap and an integrated chatbot."
+                  They wanted to move from WordPress to a custom-built site developed with React. Our team ensured the site was fully mobile-optimised and SEO-friendly, 
+                  complete with a comprehensive sitemap and an integrated chatbot."
               p2="Working with their colour palette, fonts, and images, we created a visually appealing and engaging user experience. 
-                            We where provided with their company copy to maintain brand consistency and deliver clear messaging. The result is a modern, efficient, and user-friendly website that enhances MorfBot's digital presence."
+                  We where provided with MorfBot's company copy to maintain brand consistency and deliver clear messaging. 
+                  The result is a modern, efficient, and user-friendly website that enhances MorfBot's digital presence."
               video={morfbot}
               link="https://morfbot.com/"
               p3=""
