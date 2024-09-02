0ad62c9bc9a475af682dfa19187f1fb57e2cd021
Paul <p.rossiter83@gmail.noreply.github.com>
2024-09-02 13:32:24 +0100

    [Refactored] new discovery form button added

--- src/components/CTA/index.jsx
+++ src/components/CTA/index.jsx
@@ -8,7 +8,7 @@ const CallToAction = () => {
         <h2 className="cta-title">Ready to get started?</h2>
         <p className="cta-p">
           Contact us to discuss your project, book a discovery call with us or
-          complete our client discovery form to get started.
+          complete our online client discovery form to get started.
         </p>
       </div>
       <div className="btn-container">
@@ -17,8 +17,8 @@ const CallToAction = () => {
           link="https://calendly.com/immutable-studio/website-consultancy"
           target="_blank"
         />
-        <Button text="contact us" link="/contact" background="var(--primary)" />
-        <Button text="Discovery Form" link="/discovery-form" />
+        <Button text="Discovery Form" link="/discovery-form" /> 
+        <Button text="contact us" link="/contact"/>
       </div>
     </>
   );
--- src/components/pages/home/index.jsx
+++ src/components/pages/home/index.jsx
@@ -79,6 +79,10 @@ const Home = () => {
 
             <div className="btn-container">
               <Button link="contact" text="CONTACT US" />
+               <Button
+                link="discovery-form"
+                text="discovery form"
+              />
               <Button
                 link="https://calendly.com/immutable-studio/website-consultancy"
                 text="Book a call"
