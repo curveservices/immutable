52164ed9864626e7c7baef6c028733837ec7252f
Paul <p.rossiter83@gmail.noreply.github.com>
2024-10-15 08:55:20 +0100

    [Refactored] copy changed, new util added

--- src/components/utils/accordionData/index.jsx
+++ src/components/utils/accordionData/index.jsx
@@ -5,38 +5,31 @@ export const accordionData = [
     title: "WEB DEVELOPMENT",
     subtitle: " WordPress - WooCommerce - NodeJs - NextJs - React",
     content:
-      "We specialise in designing custom built websites, offering solutions ranging from landing pages to multi-page sites. We also develop on platforms such as WordPress, Squarespace, and WebFlow.",
+      "We specialise in designing custom built websites and building on Wordpress, our offers range from landing pages to multi-page e-commerce sites. We build websites from just £540 or £50 per month",
   },
-  // {
-  //   id: 1,
-  //   link: "design-service",
-  //   title: "DESIGN SERVICES",
-  //   subtitle: "Sketch - Creative Suite  - Figma",
-  //   content:
-  //     "Our design team will produce custom website and mobile wireframe design and UX, UI design.",
-  // },
+  {
+    id: 1,
+    link: "seo-website-maintenance",
+    title: "MAINTENANCE & SEO",
+    subtitle: "Content - Security - SEO - Reporting - Hosting",
+    content:
+      "We can look after your website for you, everything from content to SEO, and we do it all in-house. Our SiteCare plans start from just £40 per month",
+  },
   {
     id: 2,
     link: "chat-bots",
     title: "AI SOLUTIONS",
-    subtitle: "VoiceFlow - Botpress - OpenAI - Anthropic",
+    subtitle: "VoiceFlow - Botpress - OpenAI - Anthropic - Automation",
     content:
       "If you are looking for AI solutions for your business we can help alleviate your pain points with multiple solutions. We will help you automate and outsource parts of your day to day work with the latest AI technology.",
   },
-  {
-    id: 3,
-    link: "digital-marketing",
-    title: "DIGITAL MARKETING",
-    subtitle: "Facebook - LinkedIn - X - Instagram",
-    content:
-      "Our experienced media content team are on hand to help you set up ad campaigns and social content. We will help drive your social projects using latest trends and analyics",
-  },
+ 
   {
     id: 4,
     link: "packages",
     title: "PACKAGES",
-    subtitle: "Multi page websites - SPA's - AI Solutions - Social Media",
+    subtitle: "Multi-page Websites - AI Solutions - Maintenance & SEO",
     content:
-      "We offer a full range of packages which include but not limited to, single & multi-page websites with full AI integration. We also include workflow automation & digital marketing",
+      "We offer a range of packages which include but not limited to, single & multi-page websites with AI integration. We also include SEO & Maintenance. Our packages start from just £849 or £99 per month.",
   },
 ];
