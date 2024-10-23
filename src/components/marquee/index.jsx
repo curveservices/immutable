0dc4587da65d76b5e6a51883997061d4e12d3e06
Paul <p.rossiter83@gmail.noreply.github.com>
2024-10-23 14:14:34 +0100

    [Refactored] noreferer added. Image width and height added

--- src/components/marquee/index.jsx
+++ src/components/marquee/index.jsx
@@ -15,34 +15,36 @@ import { Link } from 'react-router-dom';
 const MarqueeComp = () => {
     return (
       <div className='marquee'>
-        <div className="marquee-title">Web design is an art. At our Web Design Agency in London we are the artists.</div>
-        <p className='marquee-p'>We use the power of technology and make bespoke websites on all major CMS and framworks.</p>
+        <div className="text-content">
+          <div className="marquee-title">Web design is an art. At our Web Design Agency in London we are the artists.</div>
+          <p className='marquee-p'>We use the power of technology and make bespoke websites on all major CMS and framworks.</p>
+        </div>
         <Marquee pauseOnHover='true' className="marquee">
-          <Link to="https://wordpress.com/" noreferer target='_blank'>
+          <Link to="https://wordpress.com/" noreferer="true" target='_blank'>
             <img src={wordpress} alt="wordpress icon" className="icon"/>
           </Link>
           <div className="divider">|</div>
-          <Link to="https://www.squarespace.com/" noreferer target='_blank'>
+          <Link to="https://www.squarespace.com/" noreferer="true" target='_blank'>
             <img src={square} alt="squarespace icon" className="icon" />
           </Link>
           <div className="divider">|</div>
-          <Link to="https://woocommerce.com/" noreferer target='_blank'>
+          <Link to="https://woocommerce.com/" noreferer="true" target='_blank'>
             <img src={woo} alt="woo-commerce" className="icon"/>    
           </Link>
           <div className="divider">|</div>
-          <Link to="https://www.shopify.com/uk" noreferer target='_blank'>
+          <Link to="https://www.shopify.com/uk" noreferer="true" target='_blank'>
             <img src={shop} alt="shopify icon" className="icon" />
           </Link>
           <div className="divider">|</div>
-          <Link to="https://www.wix.com/" noreferer target='_blank'>
+          <Link to="https://www.wix.com/" noreferer="true" target='_blank'>
             <img src={wix} alt="wix icon" className="icon" />
           </Link>
           <div className="divider">|</div>
-          <Link to="https://react.dev/" noreferer target='_blank'>
+          <Link to="https://react.dev/" noreferer="true" target='_blank'>
             <img src={react} alt="react icon" className='icon'/>
           </Link>
           <div className="divider">|</div>
-          <Link to="https://www.godaddy.com/en-uk" noreferer target='_blank'>
+          <Link to="https://www.godaddy.com/en-uk" noreferer="true" target='_blank'>
             <img src={daddy} alt="godaddy icon" className="icon" />
           </Link>
           <div className="divider">|</div>
