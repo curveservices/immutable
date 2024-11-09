0f672dde6e1716e04af00e2caf3a5eeaaea0c790
Paul <p.rossiter83@gmail.noreply.github.com>
2024-11-09 15:57:23 +0000

    [Refactored] component re-coded and styled for new client pages

--- src/components/cards/ClientWork/index.jsx
+++ src/components/cards/ClientWork/index.jsx
@@ -1,9 +1,10 @@
 import { collection, getDocs } from "firebase/firestore";
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import ReactPlayer from "react-player/lazy";
 import { db } from "../../../firbase.config";
 import Button from "../../button";
+import CallToAction from "../../CTA";
+import PriceCard from "../basicPrice";
 import "./index.scss";
 
 const Client = (props) => {
@@ -17,14 +18,10 @@ const Client = (props) => {
         clientData.push({
           id: element.id,
           name: data.name,
+          client: data.client,
+          product: data.product,
           img1: data.img1,
           img2: data.img2,
-          img3: data.img3,
-          img4: data.img4,
-          mobile1: data.mobile1,
-          mobile2: data.mobile2,
-          mobile3: data.mobile3,
-          client: data.client,
           logo: data.logo,
           link: data.link,
           color: data.color,
@@ -40,66 +37,78 @@ const Client = (props) => {
     fetchData();
   }, []);
   return (
+    <>
     <div className="client-container">
       {client.map((item) => {
         return (
           <div key={item.id}>
             <div className="inner-container">
+              <img src={props.src} className="hero-img" alt="" />
               <div className="text-box">
-                <div className="title-container">
-                  <h2 style={{ color: item.color }}>{item.client}</h2>
-                  <Link to={item.link} target="_blank">
-                    <img src={item.logo} alt="logo" className="logo" />
-                  </Link>
-                </div>
-                <p>{props.p}</p>
-                <p>{props.p2}</p>
-
+                <h1 className="title" >{item.client}</h1>
+                <p>{props.titleP}</p>
                 <Button
-                  text="View Site"
-                  link={props.link}
-                  target="_blank"
-                  background={item.color}
-                  noreferer
-                />
-              </div>
-              <div className="player-wrapper">
-                <ReactPlayer
-                  className="react-player"
-                  url={props.video}
-                  playing
-                  loop
-                />
+                text="View Site"
+                link={props.link}
+                target="_blank"
+                background={item.color}
+                noreferer
+              />
               </div>
             </div>
-            <h2 className="name" style={{ color: item.color }}>
-              {item.name}
-            </h2>
-            <div className="images">
-              <img className="wireframe" src={item.img1} alt="wireframe" />
-              <img className="wireframe" src={item.img2} alt="wireframe" />
-              <img className="wireframe" src={item.img3} alt="wireframe" />
-              <img className="contact" src={item.img4} alt="wireframe" />
-              <img
-                className="mobile-wireframe"
-                src={item.mobile1}
-                alt="wireframe"
-              />
-              <img
-                className="mobile-wireframe"
-                src={item.mobile2}
-                alt="wireframe"
-              />
-              <img
-                className="mobile-wireframe"
-                src={item.mobile3}
-                alt="wireframe"
-              />
+            <section className="second-section" > 
+              <h2 className="name" style={{ color: item.color }}>{item.product} for {item.client}</h2>
+              <div className="top-container">
+                
+                <p>{props.p}</p>
+                <div className="player-wrapper">
+                  <ReactPlayer
+                    className="react-player"
+                    alt={`${item.client} home page on a laptop`}
+                    url={props.url}
+                    playing
+                    loop
+                    />
+                </div>
+              </div>
+              <div className="work-container">
+                <img className="client-img" src={item.img1} alt={`${item.client} homepage`} />
+                <div className="text-box">
+                  <h2 style={{ color: item.color }}>The Design</h2>
+                  <p>{props.img1P}</p>
+                  <p>{props.img1Break}</p>
+                </div>
+              </div>
+              <div className="work-container container2">
+                <div className="text-box">
+                  <h2 style={{ color: item.color }}>Our Approach</h2>
+                  <p>{props.img2P}</p>
+                  <p>{props.img2Break }</p>
+                </div>
+                  <img className="client-img" src={item.img2} alt={`${item.client} mobile view`} />
+              </div>
+            </section>
+            <section className="third-section">
+              <div className="text-box">
+                <h2>Transparent Pricing</h2>
+              <p>
+                We believe in transparency and building strong relationships
+                with our clients. We offer straightforward pricing and
+                understand the importance of budgeting. Packages can be expanded
+                as your business grows.
+              </p>
+              <h2 className="price-title">Our Starting Prices</h2>
+              </div>
+              <PriceCard name="pricing"/>
+            </section>
+            <div className="cta">
+              <CallToAction/>
             </div>
           </div>
         );
       })}
-    </div>
+      </div>
+      </>
   );
 };
 
