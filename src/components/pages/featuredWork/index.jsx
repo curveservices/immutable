97fd8b8c2b47705b5b23a348bbc1e9893e04d688
Paul <p.rossiter83@gmail.noreply.github.com>
2024-11-09 16:05:49 +0000

    [Feature] compnent re-coded and styled to create links to client pages

--- src/components/pages/featuredWork/index.jsx
+++ src/components/pages/featuredWork/index.jsx
@@ -1,29 +1,34 @@
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { useEffect, useState } from "react";
-
+import paper from "../../../assets/images/1746747.webp";
 import morfbot from "../../../assets/images/morfbot.mp4";
 import tutor from "../../../assets/images/telehill.mp4";
-import Client from "../../cards/ClientWork";
+import vive from "../../../assets/images/vive.mp4";
 import Button from "../../button";
 import CubeSpinner from "../../Animations/cubespinner";
 import PriceCard from "../../cards/fullPrice";
 import CTA from "../../CTA";
 import vid from "../../../assets/images/working.mp4";
-import working from "../../../assets/images/working.png";
-import paper from "../../../assets/images/1746747.webp";
+import working from "../../../assets/images/working.webp";
 import LazyVideo from "../../LazyVideo";
+import ReactPlayer from "react-player";
+import SlideButton from "../../buttonSlide";
 import "./index.scss";
+
 
 const FeaturedWork = () => {
   const [firstScroll, setFirstScrolll] = useState(false);
   const [secondScroll, setSecondScroll] = useState(false);
+  const [thirdScroll, setThirdScroll] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
       const scrollFirst = window.scrollY;
       const scrollSecond = window.scrollY;
-      setFirstScrolll(scrollFirst > 150);
-      setSecondScroll(scrollSecond > 1500);
+      const scrollThird = window.scrollY;
+      setFirstScrolll(scrollFirst > 250);
+      setSecondScroll(scrollSecond > 850);
+      setThirdScroll(scrollThird > 1300);
     };
     window.addEventListener("scroll", handleScroll);
   }, []);
@@ -73,53 +78,81 @@ const FeaturedWork = () => {
             backgroundPosition: "center",
           }}
         >
-          <div
-            className={`wireframe-container ${firstScroll ? "anim" : "none"}`}
-          >
-            <Client
-              name="telegraph-tutoring"
-              p="A local tutoring company contacted us for a brand new website. 
-                They wanted a fast, custom-built website developed with the latest front-end tech, using a database for learning resources to be accessed by parents.
-                We built their website to be fully optimised for mobile and tablet. We ensured this site was SEO-friendly, 
-                complete with a comprehensive sitemap and google analytics"
-              p2="We worked with them to get the colour palette, fonts, and images to create a visually appealing and engaging user experience. 
-                  We where provided with their company copy to maintain brand consistency and deliver clear messaging. 
-                  The result is a modern, efficient, and user-friendly website to enhance their digital presence."
-              video={tutor}
-              link="https://telegraphhilltutoring.netlify.app/"
-              p3=""
-            />
+          <div className="main-text">
+            <p className="client-header">OUR PORTFOLIO</p>
+            <h1>Recent Projects</h1>
+            <p>Here’s a selection of our recent projects across the digital landscape.</p>
           </div>
-        </section>
-        <section
-          className="clients"
-          lazyloading="true"
-          style={{
-            backgroundImage: `url(${paper})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-        >
-          <div
-            className={`wireframe-container ${secondScroll ? "anim" : "none"}`}
-          >
-            <Client
-              name="morfbot"
-              p="An innovative AI automation agency approached us for a complete redesign of their website. 
-                  They wanted to move from WordPress to a custom-built site developed with React. Our team ensured the site was fully mobile-optimised and SEO-friendly, 
-                  complete with a comprehensive sitemap and an integrated chatbot."
-              p2="Working with their colour palette, fonts, and images, we created a visually appealing and engaging user experience. 
-                  We where provided with MorfBot's company copy to maintain brand consistency and deliver clear messaging. 
-                  The result is a modern, efficient, and user-friendly website that enhances MorfBot's digital presence."
-              video={morfbot}
-              link="https://morfbot.netlify.app/"
-              p3=""
-            />
+          <div className={`featured-container ${firstScroll ? "anim" : "none"}`}>
+            <div className="text-box">
+              <h2>Telegraph Hill Tutoring</h2>
+              <h3>WEBSITE</h3>
+              <p>We focused on creating a modern, playful, easy to understand and eye-catching website,
+                developing a visual identity to evoke feelings of saftey, professionalism and, peace of mind.
+              </p>
+              <SlideButton text="Read case study" link="telegraph-hill-tutoring"/>
+            </div>
+            <div className={`player-wrapper ${firstScroll ? "anim" : "none"}`}>
+              <ReactPlayer
+                className="react-player"
+                url={tutor}
+                playing
+                loop
+              />
+            </div>
+          </div>
+           <div className={`featured-container ${secondScroll ? "anim" : "none"}`}>
+            <div className="text-box">
+              <h2>MorfBot</h2>
+              <h3>WEBSITE</h3>
+              <p>We join forces with MorfBot to redesign their website. We created a professionl eye-catching site, built
+                with React.js for scalability. Using their colour palette and images, evoking a serious tech feel.
+              </p>
+              <SlideButton text="Read case study" link="morfbot"/>
+            </div>
+            <div className={`player-wrapper ${secondScroll ? "anim" : "none"}`}>
+              <ReactPlayer
+                className="react-player"
+                url={morfbot}
+                playing
+                loop
+              />
+            </div>
+          </div>
+          <div className={`featured-container ${thirdScroll ? "anim" : "none"}`}>
+            <div className="text-box">
+              <h2>Vive La Crepe</h2>
+              <h3>WEBSITE</h3>
+              <p>We focused on creating a modern, playful, easy to understand and eye-catching website,
+                developing a visual identity to evoke feelings of saftey, professionalism and, peace of mind.
+              </p>
+              <SlideButton text="Read case study" link="vive-la-crepe"/>
+            </div>
+            <div className={`player-wrapper ${thirdScroll ? "anim" : "none"}`}>
+              <ReactPlayer
+                className="react-player"
+                url={vive}
+                playing
+                loop
+              />
+            </div>
+          </div>
+          <div className="container-footer">
+            <p>The ultimate worksheet to help you collect all the information, ideas and prep work you need before starting your project with us.</p>
+            <div className="btn-container">
+              <Button text="client discovery form" link="/discovery-form" />
+            </div>
+            
           </div>
         </section>
         <section className="featured">
-          <h2>WEBSITE DEVELOPMENT PRICES</h2>
-          <PriceCard name="webDev" color="#fff" />
+          <div className="text-box">
+            <h2>Our Starting Prices</h2>
+            <p>We believe in transparency and building strong relationships with our clients. We offer straightforward pricing and understand the importance of budgeting.
+            Packages can be expanded as your business grows.</p>
+          </div>
+         
+          <PriceCard name="pricing" color="#fff" />
         </section>
         <section className="cta">
           <CTA />
