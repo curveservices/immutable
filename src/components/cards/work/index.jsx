8726ebcb3e5cc328d53b3b48672e2a1878eb2382
Paul <p.rossiter83@gmail.noreply.github.com>
2024-11-11 12:36:12 +0000

    [Refactored] card recoded to flip on hover, title and p added so this component can be reused as a section on the website

--- src/components/cards/work/index.jsx
+++ src/components/cards/work/index.jsx
@@ -3,6 +3,7 @@ import { db } from "../../../firbase.config";
 import { getDocs, collection } from "firebase/firestore";
 import Button from "../../button";
 import "./index.scss";
+import { NavLink } from "react-router-dom";
 
 const WorkCard = () => {
   const [card, setCard] = useState([]);
@@ -17,8 +18,13 @@ const WorkCard = () => {
           id: element.id,
           name: data.name,
           img: data.img,
+          back: data.back,
           desc: data.desc,
           live: data.live,
+          product: data.product,
+          productLink: data.productLink,
+          color: data.color,
+          textColor: data.textColor,
         });
       });
       setCard(workData);
@@ -32,27 +38,62 @@ const WorkCard = () => {
   }, []);
 
   return (
-    <div className="card-container">
-      {card.map((item) => {
-        return (
-          <div className="card" key={item.id}>
-            <img src={item.img} alt="feature image" className="card-image" />
-            <div className="content">
-              <div className="name">{item.name}</div>
-              <p className="desc">{item.desc}</p>
-              <div className="btn-container">
-                <Button
-                  text="View"
-                  link={item.live}
-                  background="var(--services-link)"
-                  target="_blank"
-                />
+    <>
+      <h2 className="title">EXPLORE OUR PORTFOLIO</h2>
+      <div className="text-container">
+        <div className="textBox">
+          <p>
+            Signing off on a new website is always a moment of pride and
+            satisfaction for us. With each project, we take great care to
+            understand our clients' unique needs and deliver tailored
+            solutions. Here are some of our featured projects.
+          </p>
+        </div>
+      </div>
+      <div className="card-container">
+        {card.map((item) => {
+          return (
+            <div className="flip-card" key={item.id}>
+              <div className="flip-card-inner">
+                <div className="flip-card-front" >
+                  <img src={item.img} alt={`${item.name} featured images`} className="card-image"
+                    style={{width:"300px",height:"300px", }}/>
+              </div>
+                <div className="flip-card-back" style={{background: item.color}}>
+                  <img src={item.back} alt={`${item.name} Home page`} className="card-back-image" />
+                  <h1 className="name" >{item.name}</h1>
+                  <NavLink to={`/services/${item.productLink}`}>
+                    <h2 className="product">{item.product}</h2>
+                  </NavLink>
+                <p className="desc" >{item.desc}</p>
+                <div className="btn-container">
+                  <Button
+                    text="View"
+                    link={`/portfolio${item.live}`}
+                    background="var(--services-link)"
+                  />
+                </div>
+                </div>
               </div>
             </div>
-          </div>
-        );
-      })}
-    </div>
+          );
+        })}
+      </div>
+      <div className="text-container">
+        <div className="textBox">
+          <p>
+            As our client base continues to expand, we are excited to
+            showcase the diverse and innovative work we do. Each project
+            that we complete represents a commitment to excellence and our
+            passion for helping businesses thrive online.
+          </p>
+        </div>
+        <div className="btn-container">
+          <Button text="Portfolio" link="portfolio"/>
+          <Button text="Discovery Form" link="discovery-form"/>
+        </div>
+      </div>
+    </>
   );
 };
 
