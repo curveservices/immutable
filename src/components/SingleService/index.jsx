3a98b8fd64d53fede3c886f0d2181cefd5b94494
Paul <p.rossiter83@gmail.noreply.github.com>
2024-11-12 22:04:20 +0000

    [Feature] howitworks component added paper background removed

--- src/components/SingleService/index.jsx
+++ src/components/SingleService/index.jsx
@@ -4,6 +4,11 @@ import { Helmet, HelmetProvider } from "react-helmet-async";
 import Button from "../button";
 import PriceCard from "../cards/fullPrice";
 import CallToAction from "../CTA";
+import HowitWorks from "../howItWorks";
+import webdev from "../../assets/images/layout.gif";
+import meeting from "../../assets/images/online-meeting.gif";
+import chatbot from "../../assets/images/chat-bot.gif";
+import design from "../../assets/images/prototype.gif";
 import style from "./services.module.css";
 
 const Singleservices = (props) => {
@@ -54,6 +59,31 @@ const Singleservices = (props) => {
               className={`${second ? style.animSecondimg : style.secondNone}`}
             />
           </div>
+        </section>
+        <section className={style.howItWorks}>
+          <HowitWorks
+                title="How it works"
+                mainP="We’re passionate about helping our clients brand stand out online. Customer service is more than a commitment, it shapes
+                      every aspect of our business. From discovery to final delivery, we prioritise understanding your visions &amp; goals."
+                card1Gif={meeting}
+                card1Title="Discovery Call"
+                card1P="Conducting a video call helps us to get to know each other. Discovery calls are important to help us understand
+                          your projects needs."
+                card2Gif={design}
+                card2Title="Proposal and Design"
+                  card2P="Once we fully understand your project a proposal is drawn up. We then start on design, we will create wireframe UI/UX designs
+                          for approval."
+                card3Gif={webdev}
+                card3Title="Draft & Completion"
+                  card3P="As soon as the design is approved development begins. A draft will hosted for you to approve. Once approved your 
+                        project is ready for completion"
+                card4Gif={chatbot}
+                card4Title="Ongoing Support"
+                  card4P="We offer ongoing support with hosting, website maintenance and SEO development. Our support gives you peace-of-mind, we'll be
+                  on hand to help."
+                text="Explore our Portfolio"
+                link="/portfolio"
+              />
         </section>
         <section className={style.thirdSection}>
           <div className={style.thirdInner}>
