51de1cd30c5872ced1818ecea994eaed6bd24b24
Paul <p.rossiter83@gmail.noreply.github.com>
2024-11-23 10:23:30 +0000

    [Feature] compression build and purge.css added for better performance

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,65 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import compression from "vite-plugin-compression";
+import purgecss from "vite-plugin-purgecss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: { chunkSizeWarningLimit: 1600 },
+  css: {
+    modules: {
+      localsConvention: 'camelCase', // makes sure the classes are camelCase, like priceCard
+    },
+  },
+  plugins: [
+    react(),
+    purgecss({
+  content: ['./index.html', './src/**/*.{jsx,js,tsx,ts}', './public/**/*.html',],
+      safelist: {
+        standard: [
+          /^leaflet-/,  
+          'leaflet-container', 
+          'leaflet-tile', 
+          'leaflet-marker-icon', 
+          'toast', 
+          'toast-container', 
+          'toast-success', 
+          'toast-error', 
+          'fade-in', 
+          'fade-out',
+          /^toast-.*$/,
+          'ReactToastify__toast', 
+          'ReactToastify__toast-container', 
+          'ReactToastify__toast-body', 
+          'ReactToastify__progress-bar',
+          'load-spinner'
+        ],
+      }
+    }),
+    compression({
+      algorithm: "gzip",
+      ext: ".gz",
+      threshold: 1024,
+      deleteOriginFile: false,
+      filter: /\.(js|mjs|json|css|html|svg|ico|png|jpg|webp)$/,
+    }),
+    compression({
+      algorithm: "brotliCompress",
+      ext: ".br",
+      threshold: 1024,
+      filter: /\.(js|mjs|json|css|html|svg|ico|png|jpg|webp)$/,
+    }),
+  ],
+  build: {
+    minify: 'esbuild',
+    modulePreload: true,
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        }
+      }
+    }
+  },
   define: { "process.env": process.env },
 });
