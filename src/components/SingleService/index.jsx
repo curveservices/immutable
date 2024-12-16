9ac82ec6db9404650b37f4cb435d51dc42d8b50f
Paul <p.rossiter83@gmail.noreply.github.com>
2024-12-16 19:16:41 +0000

    [Feature] ai assisants page dynamically added

--- src/components/SingleService/index.jsx
+++ src/components/SingleService/index.jsx
@@ -9,24 +9,26 @@ import webdev from "../../assets/images/layout.mp4";
 import meeting from "../../assets/images/online-meeting.mp4";
 import chatbot from "../../assets/images/chat-bot.mp4";
 import design from "../../assets/images/prototype.mp4";
+import SlideButton from "../buttonSlide";
 
 const Singleservices = (props) => {
   const [isScrolled, setIsScrolled] = useState(false);
   const [second, setSecond] = useState(false);
+  const [aiSection, setAiSection] = useState(false);
   const [third, setThird] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
       const scrollPrice = window.scrollY;
       const scrollSecond = window.scrollY;
+      const scrollAiAssistants = window.scrollY;
       const scrollThird = window.scrollY;
       setSecond(scrollSecond > 150);
       setIsScrolled(scrollPrice > 1700);
+      setAiSection(scrollAiAssistants > 750)
       setThird(scrollThird > 1000);
     };
     window.addEventListener("scroll", handleScroll);
-  }, []);
-    useEffect(() => {
     import('./index.scss')
       .catch((error) => {
         console.error('error loading css',error);
@@ -41,6 +43,7 @@ const Singleservices = (props) => {
       }
     };
   }, []);
+  const aiPage = props.page === "ai";
   return (
 
     <>
@@ -77,6 +80,36 @@ const Singleservices = (props) => {
             />
           </div>
         </section>
+       {aiPage && ( 
+          <section className="ai-section">
+            <div className="subtitle"><i>Your AI Helpers</i></div>
+            <div className={`ai-inner ${aiSection ? "anim" : "none"}`}>
+              <h2 className="title">MEET OUR ASSISTANTS</h2>
+              <div className="ai-container">
+                <div className="ai-card">
+                  <h2 className="ai-name">{props.assistant1}</h2>
+                  <h3 className="ai-title">{props.title1}</h3>
+                  <img src={props.img1} className="ai-img" alt="a 3d cartoon of a girl in pink space suit" />
+                  {/* <SlideButton text="learn more" link="social-media"/> */}
+                  <p>LAUNCHING SOON</p>
+                </div>
+              <div className="ai-card">
+                <h2 className="ai-name">{props.assistant2}</h2>
+                <h3 className="ai-title">{props.title2}</h3>
+                <img src={props.img2} className="ai-img"alt="a 3d cartoon of a boy in a blue space suit" />
+                <SlideButton text="learn more" link="customer-service"/>
+              </div>
+              <div className="ai-card">
+                <h2 className="ai-name">{props.assistant3}</h2>
+                <h3 className="ai-title">{props.title3}</h3>
+                <img src={props.img3} className="ai-img"alt="a 3d cartoon of a boy in a blue space suit" />
+                {/* <SlideButton text="learn more" link="ecommerce"/> */}
+                <p>LAUNCHING SOON</p>
+              </div>
+            </div>
+          </div>
+        </section>
+        )}
         <section className="howItWorks">
           <h3 className="subtitle"><i>Our Services</i></h3>
           <div className={`${third ? "anim-third" : "third-none"}`}>
@@ -91,23 +124,22 @@ const Singleservices = (props) => {
             link1="https://calendly.com/immutable-studio/website-consultancy"
             text1="book a call"
             card2Gif={design}
-            card2Title="Proposal and Design"
-            card2P="Once we fully understand your project a proposal is drawn up. We then start on design, we will create wireframe UI/UX designs
-                      for approval."
+            card2Title="Proposal and Build"
+              card2P="Once we fully understand your needs a proposal is drawn up. We then start training your assistant on your specific needs."
             link2="portfolio"
             text2="Our clients"
             card3Gif={webdev}
             card3Title="Draft & Completion"
-            card3P="As soon as the design is approved development begins. A draft will hosted for you to approve. Once approved your 
-                    project is ready for completion."
+            card3P="As soon as the assistant is built and trained we will send for your approved. Once approved, your 
+                    assistant is ready for launch."
             link3="contact"
             text3="contact us"
             card4Gif={chatbot}
             card4Title="Ongoing Support"
-              card4P="We offer ongoing support with hosting, website maintenance and SEO development. Our support gives you peace-of-mind, we'll be
+              card4P="We offer ongoing support with fine tuning, more training and tweeking. Our support gives you peace-of-mind, we'll be
               on hand to help."
-            link4="services/seo-website-maintenance"
-            text4="maintenance & seo"
+            link4=""
+            text4="Ai & Automation"
             text="Explore our Portfolio"
             link="/portfolio"
           />
