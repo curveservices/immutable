bb85c4f22df46931a892591c6f6a6f2fb8e75270
Paul <p.rossiter83@gmail.noreply.github.com>
2024-12-16 18:40:48 +0000

    [Refactored] style props added

--- src/components/buttonSlide/index.jsx
+++ src/components/buttonSlide/index.jsx
@@ -3,10 +3,19 @@ import { NavLink } from 'react-router-dom';
 import "./index.scss";
 
 const SlideButton = (props) => {
+    const buttonStyle = {
+    background: props.background,
+    color: props.color,
+    fontSize: props.fontSize + "px",
+    boxShadow: props.boxShadow,
+  };
     return (
         <NavLink to={props.link} aria-label='link to case study'>
             <div className="btn btn-arrow-slide">
-                <button className="btn-arrow-slide-cont btn-arrow-slide-cont--blue">
+                <button
+                    style={buttonStyle}
+                    className="btn-arrow-slide-cont btn-arrow-slide-cont--blue"
+                >
                     <span className="btn-arrow-slide-circle" aria-hidden="true">
                     <span className="btn-arrow-slide-arrow btn-arrow-slide-icon"></span>
                     </span>
