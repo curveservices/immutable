af7e7455afd6046a7ad328ed4c8c67c89e65786b
Paul <p.rossiter83@gmail.noreply.github.com>
2025-01-20 09:28:46 +0000

    copy changed for new prices

--- src/components/pages/webDev/index.jsx
+++ src/components/pages/webDev/index.jsx
@@ -13,7 +13,7 @@ const WebDev = () => {
               specialise in tailoring web development solutions, ensuring
               businesses achieve online success through custom
               solutions."
-      HeroH2="We build custom responsive websites from just £540 or £50 per month"
+      HeroH2="We build custom responsive websites from just £250 or £25 per month"
       src={gif}
       secondH1="Transform your online presence"
       secondP="Frustrated with an outdated website that lacks engagement and
