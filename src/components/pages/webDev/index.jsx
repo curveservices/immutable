d05a4564ffb6ff36404bb3db52cba963d9e0283c
Paul <p.rossiter83@gmail.noreply.github.com>
2025-02-24 13:03:03 +0000

    Helmet Schema and canonicalURL added for better SEO practices

--- src/components/pages/webDev/index.jsx
+++ src/components/pages/webDev/index.jsx
@@ -5,7 +5,9 @@ import Singleservices from "../../SingleService/";
 const WebDev = () => {
   return (
     <Singleservices
+      page="webdev"
       title="Website Development"
+      canonicalUrl="https://immutable-studio.co.uk/services/web-development"
       keywords="web development, web design, website design, wordpress, woo-commerce, AI solutions, chatbots, workflow automation, Immutable Studio, greenwich, London"
       HeroH1="Website Development"
       HeroP="For a competitive edge, business elevation, and broader audience
@@ -22,6 +24,7 @@ const WebDev = () => {
       p="We offer a range of website solutions from Wordpress and Woo-commerce sites to custom built hard coded, scalable websites.
         We also offer help with maintence and hosting."
       img={img}
+      alt="Custom web development and design in Greenwich London and South East London"
       thirdH1="WEBSITE"
       thirdP="We emphasise clear communication and robust client relationships.
               Our services are based on a retainer model which include maintence, full support and hosting."
