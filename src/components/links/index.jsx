6a616d8391bd0e207f5875402fe68c4d86a6abc9
Paul <p.rossiter83@gmail.noreply.github.com>
2025-04-13 08:41:44 +0100

    Refactor Links component: integrate video icons for services, enhance layout and styles for improved user interaction

--- src/components/links/index.jsx
+++ src/components/links/index.jsx
@@ -1,27 +1,79 @@
 import { NavLink } from "react-router-dom";
+import webdev from "../../assets/images/layout.mp4";
+import seo from "../../assets/images/keywords.mp4";
+import ai from "../../assets/images/ai.mp4";
+import packing from "../../assets/images/packing.mp4";
 import "./index.scss";
 
 const Links = (props) => {
   return (
     <div className="link-container">
       <div className="service-link">
-        <NavLink to={props.webLink}>
-          <div>WEB DEVELOPMENT</div>
+        <NavLink to={props.dev}>
+          <div className="link">
+            <video
+            src={webdev}
+            className="icon"
+            autoPlay
+            loop
+            muted
+            playsInline
+            controls={false}
+          >  
+          </video>
+          <div className="text">WEB DEVELOPMENT</div>
+          </div>
         </NavLink>
       </div>
       <div className="service-link">
-        <NavLink to={props.maintenanceLink}>
-          <div>SEO & MAINTENANCE</div>
+        <NavLink to={props.seo}>
+          <div className="link">
+            <video
+            src={seo}
+            className="icon"
+            autoPlay
+            loop
+            muted
+            playsInline
+            controls={false}
+            >  
+            </video>
+            <div className="text">SEO & MAINTENANCE</div>
+          </div>
         </NavLink>
       </div>
       <div className="service-link">
-        <NavLink to={props.aiLink}>
-          <div>AI SOLUTIONS</div>
+        <NavLink to={props.ai}>
+          <div className="link">
+            <video
+            src={ai}
+            className="icon"
+            autoPlay
+            loop
+            muted
+            playsInline
+            controls={false}
+            >  
+            </video>
+            <div className="text">AI SOLUTIONS</div>
+          </div>
         </NavLink>
       </div>
       <div className="service-link">
-        <NavLink to={props.packagesLink}>
-          <div>PACKAGES</div>
+        <NavLink to={props.pack}>
+          <div className="link">
+            <video
+            src={packing}
+            className="icon"
+            autoPlay
+            loop
+            muted
+            playsInline
+            controls={false}
+            >  
+            </video>
+            <div className="text">PACKAGES</div>
+          </div>
         </NavLink>
       </div>
     </div>
