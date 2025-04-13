80f9f3795314cb79c678d6dcca0d270b0401718c
Paul <p.rossiter83@gmail.noreply.github.com>
2025-04-13 16:12:30 +0100

    Add second image to WebDev component for enhanced visual content

--- src/pages/webDev/index.jsx
+++ src/pages/webDev/index.jsx
@@ -1,4 +1,5 @@
 import img from "../../assets/images/service-1.webp";
+import img2 from "../../assets/images/morfbot-head.webp";
 import gif from "../../assets/images/webDev.gif";
 import Singleservices from "../../components/SingleService";
 
@@ -24,6 +25,7 @@ const WebDev = () => {
       p="We offer a range of website solutions from Wordpress and Woo-commerce sites to custom built hard coded, scalable websites.
         We also offer help with maintence and hosting."
       img={img}
+      img2={img2}
       alt="Custom web development and design in Greenwich London and South East London"
       thirdH1="WEBSITE"
       thirdP="We emphasise clear communication and robust client relationships.
