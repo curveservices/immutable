7db3addfaf6fda83af0e807f6e419ec0ee0e93a7
Paul <p.rossiter83@gmail.noreply.github.com>
2025-04-17 06:51:56 +0100

    Refactor SingleService styles for improved layout and responsiveness

--- src/components/SingleService/index.jsx
+++ src/components/SingleService/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 
 import Button from "../button";
@@ -10,25 +10,13 @@ import meeting from "../../assets/images/online-meeting.mp4";
 import chatbot from "../../assets/images/chat-bot.mp4";
 import design from "../../assets/images/prototype.mp4";
 import SlideButton from "../buttonSlide";
+import useScrollStates from "../scrollState";
+import WorkCard from "../cards/work";
 
 const Singleservices = (props) => {
-  const [isScrolled, setIsScrolled] = useState(false);
-  const [second, setSecond] = useState(false);
-  const [aiSection, setAiSection] = useState(false);
-  const [third, setThird] = useState(false);
+  const { second, third, forth } = useScrollStates();
 
   useEffect(() => {
-    const handleScroll = () => {
-      const scrollPrice = window.scrollY;
-      const scrollSecond = window.scrollY;
-      const scrollAiAssistants = window.scrollY;
-      const scrollThird = window.scrollY;
-      setSecond(scrollSecond > 150);
-      setIsScrolled(scrollPrice > 1700);
-      setAiSection(scrollAiAssistants > 750);
-      setThird(scrollThird > 1000);
-    };
-    window.addEventListener("scroll", handleScroll);
     import("./index.scss").catch((error) => {
       console.error("error loading css", error);
     });
@@ -45,12 +33,12 @@ const Singleservices = (props) => {
   const aiPage = props.page === "ai";
   const webdevPage = props.page === "webdev";
   const maintenancePage = props.page === "maintenance";
-  const packagePage = props.page === "package";
+  const packagePage = props.page === "packages";
   return (
     <>
       <div className="single-service">
         <section className="hero">
-          <div className="inner-hero">
+          <div className="hero-inner">
             <div className="text-box">
               <h1 className="title">{props.HeroH1}</h1>
               <p>{props.HeroP}</p>
@@ -71,160 +59,166 @@ const Singleservices = (props) => {
         </section>
         <section className="second-section">
           <div className="second-inner">
-            <div className="top-container">
+            <div className={`top-container ${second ? "anim" : "none"}`}>
               <div className="second-text">
-                <i className="subtitle">Web Development</i>
+                <i className="subtitle">What we do for you</i>
                 <h2 className="mian-title">{props.secondH1}</h2>
                 <p>{props.secondP}</p>
                 <p>{props.p}</p>
               </div>
               <div className="image-container">
                 <div className="square-shape"></div>
-                <img src={props.img} alt={props.alt} className="image" style={{marginBottom: '60px'}} />
-                <img src={props.img2} alt={props.alt} className="image" style={{ marginTop: '60px' }} />
+                <img
+                  src={props.img}
+                  alt={props.alt}
+                  className="image"
+                  style={{ marginBottom: "60px" }}
+                />
+                <img
+                  src={props.img2}
+                  alt={props.alt}
+                  className="image"
+                  style={{ marginTop: "60px" }}
+                />
                 <div className="circle-shape"></div>
               </div>
             </div>
-          </div>
-          {(!aiPage && (webdevPage || maintenancePage || packagePage)) && (
-        <div className="howItWorks">
-          <div className="subtitle">
-            <i>Our Services</i>
-          </div>
-          <div className={`${third ? "anim-third" : "third-none"}`}>
-            <HowitWorks
-              title="How it works"
-              mainP="We’re passionate about helping our clients brand stand out online. Customer service is more than a commitment, it shapes
+            {!aiPage && (webdevPage || maintenancePage || packagePage) && (
+              <div className="howItWorks">
+                <i className="subtitle">how it works</i>
+                <div className={`${second ? "anim" : "none"}`}>
+                  <HowitWorks
+                    title="How it works"
+                    mainP="We’re passionate about helping our clients brand stand out online. Customer service is more than a commitment, it shapes
                     every aspect of our business. From discovery to final delivery, we prioritise understanding your visions &amp; goals."
-              card1Gif={meeting}
-              card1Title="Discovery Call"
-              card1P="Conducting a video call helps us to get to know each other. Discovery calls are important to help us understand
+                    card1Gif={meeting}
+                    card1Title="Discovery Call"
+                    card1P="Conducting a video call helps us to get to know each other. Discovery calls are important to help us understand
                         your projects needs."
-              link1="https://calendly.com/immutable-studio/website-consultancy"
-              text1="book a call"
-              card2Gif={design}
-              card2Title="Proposal and Design"
-              card2P="Once we fully understand your project a proposal is drawn up. We then start on design, we will create wireframe UI/UX designs
+                    link1="https://calendly.com/immutable-studio/website-consultancy"
+                    text1="book a call"
+                    card2Gif={design}
+                    card2Title="Proposal and Design"
+                    card2P="Once we fully understand your project a proposal is drawn up. We then start on design, we will create wireframe UI/UX designs
                         for approval."
-              link2="/portfolio"
-              text2="our clients"
-              card3Gif={webdev}
-              card3Title="Draft & Completion"
-              card3P="As soon as the design is approved development begins. A draft will hosted for you to approve. Once approved your 
+                    link2="/portfolio"
+                    text2="our clients"
+                    card3Gif={webdev}
+                    card3Title="Draft & Completion"
+                    card3P="As soon as the design is approved development begins. A draft will hosted for you to approve. Once approved your 
                       project is ready for completion"
-              link3="/contact"
-              text3="contact us"
-              card4Gif={chatbot}
-              card4Title="Ongoing Support"
-              card4P="We offer ongoing support with hosting, website maintenance and SEO development. Our support gives you peace-of-mind, we'll be
+                    link3="/contact"
+                    text3="contact us"
+                    card4Gif={chatbot}
+                    card4Title="Ongoing Support"
+                    card4P="We offer ongoing support with hosting, website maintenance and SEO development. Our support gives you peace-of-mind, we'll be
                 on hand to help."
-              link4="seo-website-maintenance"
-              text4="maintenance & SEO"
-              text="Explore our Portfolio"
-              link="/portfolio"
-            />
-          </div>
-        </div>
-        )}
-        {aiPage && (
-          <>
-          <div className="ai-section">
-            <div className="subtitle">
-              <i>Your AI Helpers</i>
-            </div>
-            <div className={`ai-inner ${aiSection ? "anim" : "none"}`}>
-              <h2 className="title">MEET OUR ASSISTANTS</h2>
-              <div className="ai-container">
-                <div className="ai-card launching">
-                  <h2 className="ai-name launching">{props.assistant1}</h2>
-                  <h3 className="ai-title launching">{props.title1}</h3>
-                  <img
-                    src={props.img1}
-                    className="ai-img launching"
-                    alt="a 3d cartoon of a girl in pink space suit"
+                    link4="/services/seo-website-maintenance"
+                    text4="maintenance & SEO"
+                    text="Explore our Portfolio"
+                    link="/portfolio"
                   />
-                  {/* <SlideButton text={`more About ${props.assistant1}`} link="social-media"/> */}
-                  <p>LAUNCHING SOON</p>
                 </div>
-                <div className="ai-card">
-                  <h2 className="ai-name">{props.assistant2}</h2>
-                  <h3 className="ai-title">{props.title2}</h3>
-                  <img
-                    src={props.img2}
-                    className="ai-img"
-                    alt="a 3d cartoon of a boy in a blue space suit"
+                <WorkCard background="var(--second-section)" />
+              </div>
+            )}
+            {aiPage && (
+              <div className="howItWorks">
+                <i className="subtitle">how it works</i>
+                <div className={`${second ? "anim" : "none"}`}>
+                  <HowitWorks
+                    title="How it works"
+                    mainP="We’re passionate about helping our clients brand stand out online. Customer service is more than a commitment, it shapes
+                    every aspect of our business. From discovery to final delivery, we prioritise understanding your visions &amp; goals."
+                    card1Gif={meeting}
+                    card1Title="Discovery Call"
+                    card1P="Conducting a video call helps us to get to know each other. Discovery calls are important to help us understand
+                        your projects needs."
+                    link1="https://calendly.com/immutable-studio/website-consultancy"
+                    text1="book a call"
+                    card2Gif={design}
+                    card2Title="Proposal and Build"
+                    card2P="Once we fully understand your needs a proposal is drawn up. We then start training your assistant on your specific needs."
+                    link2="/portfolio"
+                    text2="Our clients"
+                    card3Gif={webdev}
+                    card3Title="Draft & Completion"
+                    card3P="As soon as the assistant is built and trained we will send for your approved. Once approved, your 
+                      assistant is ready for launch."
+                    link3="/contact"
+                    text3="contact us"
+                    card4Gif={chatbot}
+                    card4Title="Ongoing Support"
+                    card4P="We offer ongoing support with fine tuning, more training and tweeking. Our support gives you peace-of-mind, we'll be
+                on hand to help."
+                    link4="/services"
+                    text4="mainenance"
+                    text="Explore our Portfolio"
+                    link="/portfolio"
                   />
-                  <SlideButton text={`more About ${props.assistant2}`} link="customer-service" />
                 </div>
-                <div className="ai-card launching">
-                  <h2 className="ai-name launching">{props.assistant3}</h2>
-                  <h3 className="ai-title launching">{props.title3}</h3>
-                  <img
-                    src={props.img3}
-                    className="ai-img launching"
-                    alt="a 3d cartoon of a boy in a blue space suit"
-                  />
-                  {/* <SlideButton text={`more About ${props.assistant3}`} link="ecommerce"/> */}
-                  <p>LAUNCHING SOON</p>
+                <div className="ai-section">
+                  <div className="subtitle">
+                    <i>Your AI Helpers</i>
+                  </div>
+                  <div className={`ai-inner ${second ? "anim" : "none"}`}>
+                    <h2 className="main-title">Meet your Assistants</h2>
+                    <div className="ai-container">
+                      <div className="ai-card launching">
+                        <h2 className="ai-name launching">
+                          {props.assistant1}
+                        </h2>
+                        <h3 className="ai-title launching">{props.title1}</h3>
+                        <img
+                          src={props.img1}
+                          className="ai-img launching"
+                          alt="a 3d cartoon of a girl in pink space suit"
+                        />
+                        {/* <SlideButton text={`more About ${props.assistant1}`} link="social-media"/> */}
+                        <p>LAUNCHING SOON</p>
+                      </div>
+                      <div className="ai-card">
+                        <h2 className="ai-name">{props.assistant2}</h2>
+                        <h3 className="ai-title">{props.title2}</h3>
+                        <img
+                          src={props.img2}
+                          className="ai-img"
+                          alt="a 3d cartoon of a boy in a blue space suit"
+                        />
+                        <SlideButton
+                          text={`more About ${props.assistant2}`}
+                          link="customer-service"
+                        />
+                      </div>
+                      <div className="ai-card launching">
+                        <h2 className="ai-name launching">
+                          {props.assistant3}
+                        </h2>
+                        <h3 className="ai-title launching">{props.title3}</h3>
+                        <img
+                          src={props.img3}
+                          className="ai-img launching"
+                          alt="a 3d cartoon of a boy in a blue space suit"
+                        />
+                        {/* <SlideButton text={`more About ${props.assistant3}`} link="ecommerce"/> */}
+                        <p>LAUNCHING SOON</p>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
+            )}
           </div>
-          <div className="howItWorks">
-          <div className="subtitle">
-            <i>Our Services</i>
-          </div>
-          <div className={`${third ? "anim-third" : "third-none"}`}>
-            <HowitWorks
-              title="How it works"
-              mainP="We’re passionate about helping our clients brand stand out online. Customer service is more than a commitment, it shapes
-                  every aspect of our business. From discovery to final delivery, we prioritise understanding your visions &amp; goals."
-              card1Gif={meeting}
-              card1Title="Discovery Call"
-              card1P="Conducting a video call helps us to get to know each other. Discovery calls are important to help us understand
-                      your projects needs."
-              link1="https://calendly.com/immutable-studio/website-consultancy"
-              text1="book a call"
-              card2Gif={design}
-              card2Title="Proposal and Build"
-              card2P="Once we fully understand your needs a proposal is drawn up. We then start training your assistant on your specific needs."
-              link2="portfolio"
-              text2="Our clients"
-              card3Gif={webdev}
-              card3Title="Draft & Completion"
-              card3P="As soon as the assistant is built and trained we will send for your approved. Once approved, your 
-                    assistant is ready for launch."
-              link3="contact"
-              text3="contact us"
-              card4Gif={chatbot}
-              card4Title="Ongoing Support"
-              card4P="We offer ongoing support with fine tuning, more training and tweeking. Our support gives you peace-of-mind, we'll be
-              on hand to help."
-              link4=""
-              text4="Ai & Automation"
-              text="Explore our Portfolio"
-              link="/portfolio"
-            />
-          </div>
-          </div>
-          </>
-        )}
         </section>
-        
         <section className="third-section">
-          <div className="third-inner">
+          <div className={`third-inner ${third ? "anim" : "none"}`}>
             <div className="text-box">
-              <h2>OUR {props.thirdH1} PRICES</h2>
+              <i className="subtitle">Transparent pricing</i>
+              <h2 className="titie ">Our {props.thirdH1} Prices</h2>
               <p>{props.thirdP}</p>
             </div>
-            <div className={`${isScrolled ? "contentAnim" : "none"}`}>
-              <PriceCard
-                name={props.name}
-                color="#fff"
-                link="/discovery-form"
-              />
-            </div>
           </div>
+          <PriceCard name={props.name} link="/discovery-form" />
         </section>
         <section className="cta">
           <CallToAction />
@@ -234,18 +228,43 @@ const Singleservices = (props) => {
         <HelmetProvider>
           <Helmet>
             <title>
-              {props.title} | Immutable Studio – Creative Web Design & AI Solutions
+              {props.title} | Immutable Studio – Creative Web Design & AI
+              Solutions
             </title>
-            <link rel="canonical" href={props.canonicalUrl || window.location.href} />
-            <meta name="description" content="Immutable Studio offers creative web design, fast, responsive websites, and AI solutions for businesses." />
+            <link
+              rel="canonical"
+              href={props.canonicalUrl || window.location.href}
+            />
+            <meta
+              name="description"
+              content="Immutable Studio offers creative web design, fast, responsive websites, and AI solutions for businesses."
+            />
             <meta name="keywords" content={props.keywords} />
             <meta http-equiv="content-language" content="en" />
 
-            <meta property="og:title" content={props.title + " | Immutable Studio – Creative Web Design & AI Solutions"} />
-            <meta property="og:description" content={props.description || "Immutable Studio offers creative web design, fast, responsive websites, and AI solutions for businesses."} />
-            <meta property="og:url" content={props.canonicalUrl || window.location.href} />
+            <meta
+              property="og:title"
+              content={
+                props.title +
+                " | Immutable Studio – Creative Web Design & AI Solutions"
+              }
+            />
+            <meta
+              property="og:description"
+              content={
+                props.description ||
+                "Immutable Studio offers creative web design, fast, responsive websites, and AI solutions for businesses."
+              }
+            />
+            <meta
+              property="og:url"
+              content={props.canonicalUrl || window.location.href}
+            />
             <meta property="og:type" content="website" />
-            <meta property="og:image" content="https://immutable-studio.co.uk/1-removebg-preview.OTVxQdl4.webp" />
+            <meta
+              property="og:image"
+              content="https://immutable-studio.co.uk/1-removebg-preview.OTVxQdl4.webp"
+            />
           </Helmet>
         </HelmetProvider>
       </div>
