d9153800bbe2275ed77a746de8fac09f54810027
Paul <p.rossiter83@gmail.noreply.github.com>
2025-05-08 09:46:31 +0100

    Refactor WorkCard layout and styling for improved readability and structure

--- src/components/cards/work/index.jsx
+++ src/components/cards/work/index.jsx
@@ -42,16 +42,42 @@ const WorkCard = (props) => {
   return (
     <>
       <div className="profile-text">
-
+        <div className="title">
           <i className="subtitle">our portfolio</i>
           <h2 className="main-title">Explore Our Portfolio</h2>
+        </div>
+        <div className="text-box">
+          <div className="left">
+            <i className="subtitle">Real results. Real businesses.</i>
             <p>
               Signing off on a new website is always a moment of pride and
               satisfaction for us. With each project, we take great care to
-              understand our clients' unique needs and deliver tailored solutions.
-              Here are some of our featured projects.
+              understand our clients' unique needs and deliver tailored
+              solutions. Here are some of our featured projects.
             </p>
+          </div>
 
+          <div className="right">
+            <i className="subtitle" style={{ textAlign: "center" }}>
+              Designed for impact.
+            </i>
+            <p>
+              We are excited to showcase the diverse and innovative work we do.
+              Each project that we complete represents a commitment to
+              excellence and our passion for helping businesses thrive online.
+            </p>
+          </div>
+        </div>
+
+        <div className="btn-container">
+          <Button text="Our Portfolio" link="/portfolio" color="#000" />
+          <Button
+            text="Discovery Form"
+            link="/discovery-form"
+            background="var(--third-bg)"
+            color="#fff"
+          />
+        </div>
       </div>
       <div className="card-container">
         {card.map((item) => {
@@ -69,9 +95,7 @@ const WorkCard = (props) => {
                     <h3 className="name">{item.name}</h3>
                   </div>
                 </div>
-                <div
-                  className="flip-card-back"
-                >
+                <div className="flip-card-back">
                   <img
                     src={item.back}
                     alt={`${item.name} Home page`}
@@ -87,7 +111,7 @@ const WorkCard = (props) => {
                     <Button
                       text="View"
                       link={`/portfolio${item.live}`}
-                      background='var(--third-bg)'
+                      background="var(--third-bg)"
                       color="#fff"
                     />
                   </div>
@@ -98,25 +122,7 @@ const WorkCard = (props) => {
         })}
       </div>
       <div className="bottom-container">
-        <div className="textBox">
-          <i className="subtitle" style={{textAlign: "center"}}>
-            As our client base continues to expand
-          </i>
-          <p>We are excited to showcase
-            the diverse and innovative work we do. Each project that we complete
-            represents a commitment to excellence and our passion for helping
-            businesses thrive online.</p>
-           <div className="btn-container">
-          <Button text="Our Portfolio" link="/portfolio" color='#000' />
-          <Button
-            text="Discovery Form"
-            link="/discovery-form"
-            background="var(--third-bg)"
-            color="#fff"
-          />
-        </div>
-        </div>
-       
+        <div className="textBox"></div>
       </div>
     </>
   );
