2291fe11dadff34be66111405d1bda7ef7a13627
Paul <p.rossiter83@gmail.noreply.github.com>
2025-05-13 11:15:15 +0100

    Refactor TeamCard component: update layout and styling for improved presentation and responsiveness

--- src/components/cards/teamCard/index.jsx
+++ src/components/cards/teamCard/index.jsx
@@ -32,25 +32,49 @@ const TeamCard = () => {
   }, []);
 
   return (
-    <div className="team-container">
-      {teamCard.map((item) => {
-        return (
-          <div className="team-card" key={item.id}>
-            <img className="teamImg" src={item.img} alt="team member" />
-            <div className="text-box">
-              <h1 className="name">{item.name}</h1>
-              <h2 className="job">{item.job}</h2>
-              <p className="desc">{item.desc}</p>
+    <>
+      <div className="subtitle">
+        <i>About us</i>
+      </div>
+      <h2 className="main-title">Meet The Team</h2>
+      <div className="textBox">
+        <div className="left">
+          <div className="subtitle">UK Web Experts</div>
+          <p>
+            With more than 15 years experience, we’ve designed, built and
+            managed bespoke websites and advertising campaigns for companies
+            across the UK.
+          </p>
+        </div>
+        <div className="right">
+          <div className="subtitle">Straight-Talking Support</div>
+          <p>
+            Our friendly team won’t overwhelm you with technical jargon or
+            suggest expensive solutions that aren’t appropriate. We listen to
+            your needs and provide advice in plain English.
+          </p>
+        </div>
+      </div>
+      <div className="team-container">
+        {teamCard.map((item) => {
+          return (
+            <div className="team-card" key={item.id}>
+              <img className="teamImg" src={item.img} alt="team member" />
+              <div className="text-box">
+                <h2 className="name">{item.name}</h2>
+                <h3 className="job subtitle">{item.job}</h3>
+                <h4 className="desc">{item.desc}</h4>
+              </div>
+              <Socials
+                fblink="https://www.facebook.com/profile.php?id=61557552873479"
+                lilink="https://www.linkedin.com/company/21439623"
+                instalink="https://www.instagram.com/immutable_studio/"
+              />
             </div>
-            <Socials
-              fblink="https://www.facebook.com/profile.php?id=61557552873479"
-              lilink="https://www.linkedin.com/company/21439623"
-              xlink="https://twitter.com/immutable_LDN"
-            />
-          </div>
-        );
-      })}
-    </div>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
