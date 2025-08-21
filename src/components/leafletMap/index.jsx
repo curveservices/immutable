fca5eb580c3ac1ace65819d6b4833894318c5def
Paul <p.rossiter83@gmail.noreply.github.com>
2025-08-21 15:46:05 +0100

    Add Leaflet import to LeafletMap component for map functionality

--- src/components/leafletMap/index.jsx
+++ src/components/leafletMap/index.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef } from 'react';
+import L from 'leaflet';
 import "leaflet/dist/leaflet.css";
 
 const LeafletMap = () => {
